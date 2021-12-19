4ab4f476fdd252e0862e3a6efa4155bb3182f809
Samefral <galchun174@mail.ru>
2021-12-19 13:19:53 +0500

    image optimization

--- products_db.php
+++ products_db.php
@@ -4,7 +4,7 @@ function get_products()
   return [
     '1' => [
       'title' => 'GeForce GTX 1060',
-      'img_url' => '/images/Geforce_gtx_1060.jpg',
+      'img_url' => '/images/geforce_gtx_1060.jpg',
       'discount' => 5000,
       'price' => 21300,
       'is_new' => false,
@@ -13,7 +13,7 @@ function get_products()
     ],
     '2' => [
       'title' => 'Radeon RX 570',
-      'img_url' => '/images/Radeon570.jpg',
+      'img_url' => '/images/radeon_rx_570.jpg',
       'discount' => 200,
       'price' => 13000,
       'is_new' => true,
@@ -22,7 +22,7 @@ function get_products()
     ],
     '3' => [ 
       'title' => 'GeForce GTX 1050 Ti',
-      'img_url' => '/images/1050Ti.jpg',
+      'img_url' => '/images/geforce_gtx_1050_ti.jpg',
       'discount' => 0,
       'price' => 12000,
       'is_new' => false,
@@ -30,17 +30,17 @@ function get_products()
       'type' => 'Nvidia',
     ],
     '4' => [
-      'title' => 'GeForce RTX 2060 SUPER',
-      'img_url' => '/images/GeForce_RTX_2060.jpg',
-      'discount' => 36000,
-      'price' => 110000,
+      'title' => 'GeForce RTX 2060',
+      'img_url' => '/images/geforce_rtx_2060.jpg',
+      'discount' => 7000,
+      'price' => 42000,
       'is_new' => true,
       'is_last' => true,
       'type' => 'Ndivia',
     ],
     '5' => [
       'title' => 'GeForce GTX 1660 SUPER',
-      'img_url' => '/images/GeForce GTX 1660 SUPER.jpg',
+      'img_url' => '/images/geforce_gtx_1660_super.jpg',
       'discount' => 0,
       'price' => 18000,
       'is_new' => false,
@@ -49,7 +49,7 @@ function get_products()
     ],
     '6' => [
       'title' => 'Radeon R9 380X',
-      'img_url' => '/images/Radeon R9 380X.jpg',
+      'img_url' => '/images/radeon_r9_380x.jpg',
       'discount' => 0,
       'price' => 17200,
       'is_new' => false,
@@ -58,7 +58,7 @@ function get_products()
     ],
     '7' => [
       'title' => 'Radeon RX 480',
-      'img_url' => '/images/Radeon RX 480.jpg',
+      'img_url' => '/images/radeon_rx_480.jpg',
       'discount' => 0,
       'price' => 19500,
       'is_new' => false,
@@ -67,7 +67,7 @@ function get_products()
     ],
     '8' => [
       'title' => 'GeForce GTX 960',
-      'img_url' => '/images/GeForce GTX 960.jpg',
+      'img_url' => '/images/geforce_gtx_960.jpg',
       'discount' => 1500,
       'price' => 17370,
       'is_new' => true,
@@ -76,7 +76,7 @@ function get_products()
     ],
     '9' => [
       'title' => 'GeForce GTX 1070',
-      'img_url' => '/images/GeForce GTX 1070.jpg',
+      'img_url' => '/images/geforce_gtx_1070.webp',
       'discount' => 0,
       'price' => 17200,
       'is_new' => true,
@@ -85,16 +85,16 @@ function get_products()
     ],
     '10' => [
       'title' => 'GeForce RTX 2060 SUPER',
-      'img_url' => '/images/GeForce RTX2060 SUPER.jpg',
-      'discount' => 7000,
-      'price' => 42000,
+      'img_url' => '/images/geforce_rtx_2060_super.jpg',
+      'discount' => 36000,
+      'price' => 110000,
       'is_new' => false,
       'is_last' => true,
       'type' => 'Nvidia',
     ],
     '11' => [
       'title' => 'AXRX VEGA 64',
-      'img_url' => '/images/AXRX VEGA 64.jpg',
+      'img_url' => '/images/axrx_vega_64.jpg',
       'discount' => 0,
       'price' => 41000,
       'is_new' => true,
@@ -103,7 +103,7 @@ function get_products()
     ],
     '12' => [
       'title' => 'Radeon HD 2900 Pro',
-      'img_url' => '/images/Radeon HD 2900 Pro.jpg',
+      'img_url' => '/images/radeon_hd2900_pro.jpg',
       'discount' => 0,
       'price' => 7200,
       'is_new' => false,
@@ -112,7 +112,7 @@ function get_products()
     ],
     '13' => [
       'title' => 'Ultra Durable 2 GV-N950OC-2GD',
-      'img_url' => '/images/Ultra Durable 2 GV-N950OC-2GD.jpg',
+      'img_url' => '/images/ultra_durable_2.jpg',
       'discount' => 0,
       'price' => 12480,
       'is_new' => false,
@@ -121,7 +121,7 @@ function get_products()
     ],
     '14' => [
       'title' => 'Radeon RX 6800',
-      'img_url' => '/images/Radeon RX 6800.jpg',
+      'img_url' => '/images/radeon_rx_6800.jpg',
       'discount' => 10000,
       'price' => 154000,
       'is_new' => true,
@@ -130,7 +130,7 @@ function get_products()
     ],
     '15' => [
       'title' => 'Radeon RX 5500',
-      'img_url' => '/images/Radeon RX 5500 XT.png',
+      'img_url' => '/images/radeon_rx_5500_xt.jpg',
       'discount' => 0,
       'price' => 47000,
       'is_new' => false,
@@ -139,7 +139,7 @@ function get_products()
     ],
     '16' => [
       'title' => 'Radeon RX 6700 XT',
-      'img_url' => '/images/Radeon RX 6700 XT.png',
+      'img_url' => '/images/radeon_rx_6700_xt.jpg',
       'discount' => 0,
       'price' => 120000,
       'is_new' => false,
